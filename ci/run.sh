4cdbd5d6044546559f10549ba16b37b8ca9272f7
Wolfgang Dietersdorfer <wolfgang.dietersdorfer@drei.com>
2021-07-30 00:37:18 +0200

    code cleanup

--- ci/run.sh
+++ ci/run.sh
@@ -3,4 +3,4 @@
 docker container rm -f gamesbackend
 #docker run -d -it --name nodeplusora -p 9001:9000 --entrypoint /bin/bash nodeplusora:0.1
 docker run -d -it --name gamesbackend -p 9001:9000 -e dbPassword="1Blerl920la!" woifal/gamesbackend:0.2
-docker exec -it gamesbackend /bin/bash
+#docker exec -it gamesbackend /bin/bash

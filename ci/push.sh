a8c7ec124e298e15cff454f613e578266aaf1b99
Wolfgang Dietersdorfer <wolfgang.dietersdorfer@drei.com>
2021-08-21 02:36:22 +0200

    version bump

--- ci/push.sh
+++ ci/push.sh
@@ -1,3 +1,3 @@
-docker push woifal/gamesbackend:0.9
-docker push woifal/gameswsserver:0.9
-docker push woifal/gamesapp:0.9
+docker push woifal/gamesbackend:1.0
+docker push woifal/gameswsserver:1.0
+docker push woifal/gamesapp:1.0

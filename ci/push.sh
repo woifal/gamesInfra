51bb9e8a0405015e9e8fd978b28fea0e2de1cbf8
Wolfgang Dietersdorfer <wolfgang.dietersdorfer@drei.com>
2021-10-26 01:49:34 +0200

    new release 2021-10-26

--- ci/push.sh
+++ ci/push.sh
@@ -1,5 +1,5 @@
-docker push woifal/gamesbackend:1.3
-docker push woifal/gamesbackend_dev:1.3
+docker push woifal/gamesbackend:1.6
+docker push woifal/gamesbackend_dev:1.6
 
-docker push woifal/gameswsserver:1.3
-docker push woifal/gamesapp:1.3
+docker push woifal/gameswsserver:1.5
+docker push woifal/gamesapp:1.6
--- ci/run.sh
+++ ci/run.sh
@@ -1,19 +1,49 @@
 
+alias containercmd=docker
+
 # !!!!
 #sudo iptables -I INPUT -m state --state NEW -p tcp --dport 9001 -j ACCEPT
 #sudo iptables -I INPUT -m state --state NEW -p tcp --dport 9002 -j ACCEPT
 
-podman container rm -f gamesbackend
+containercmd container rm -f gamesbackend
 #docker run -d -it --name nodeplusora -p 9001:9000 --entrypoint /bin/bash nodeplusora:0.1
-podman run -d -it --name gamesbackend -e dbPassword=${dbPassword}  -p 9001:9000 woifal/gamesbackend:1.1
+containercmd run -d -it --name gamesbackend -e dbPassword_prod=${dbPassword_prod}  -p 9005:9000 woifal/gamesbackend:1.6
 #docker exec -it gamesbackend /bin/bash
 
-podman container rm -f gameswsserver
+containercmd container rm -f gamesbackenddev
 #docker run -d -it --name nodeplusora -p 9001:9000 --entrypoint /bin/bash nodeplusora:0.1
-podman run -d -it --name gameswsserver -p 9002:8044 woifal/gameswsserver:1.1
+containercmd run -d -it --name gamesbackenddev -e dbPassword_dev=${dbPassword_dev}  -p 9005:9000 woifal/gamesbackend_dev:1.6
 #docker exec -it gamesbackend /bin/bash
 
-podman container rm -f gamesapp
+containercmd container rm -f gameswsserver
+#docker run -d -it --name nodeplusora -p 9001:9000 --entrypoint /bin/bash nodeplusora:0.1
+containercmd run -d -it --name gameswsserver -p 9002:8044 woifal/gameswsserver:1.5
+#docker exec -it gamesbackend /bin/bash
+
+containercmd container rm -f gamesapp
 #docker run -d -it --name gamesapp --entrypoint /bin/bash woifal/gamesapp:0.5
-podman run -d -it --name gamesapp -e PORT=80 -p 80:80 -dt woifal/gamesapp:1.1
+containercmd run -d -it --name gamesapp -e PORT=80 -p 80:80 -dt woifal/gamesapp:1.6
 #docker exec -it gamesbackend /bin/bash
+
+
+
+#
+#
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+#   NEED TO INSTALL "kill-port" in backend image!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!
+#
+#
+#
+#
+#
